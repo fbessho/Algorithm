ee39adfd23873e7b912f1938b0580be06b622dd5
Fumihiro Bessho <iwabako@gmail.com>
2014-06-18 07:49:41 +0900

    Add POJ/AB Problem

---
+++ src/poj/ABProblem/Main.java
@@ -0,0 +1,16 @@
+package poj.ABProblem;
+
+import java.util.Scanner;
+
+/**
+ * POJ problem (id: 1000)
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt(), b = cin.nextInt();
+        System.out.println(a + b);
+    }
+}

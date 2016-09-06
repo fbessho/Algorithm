71d0c0f1d720e1cd994d74da6368879dcb17010c
Fumihiro Bessho (Ben) <fumihiro.bessho@gmail.com>
2016-09-07 04:09:34 +0900

    Fix bubblesort bug

--- cpp/sorting/bubblesort.cpp
+++ cpp/sorting/bubblesort.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include "util.h"
 
 using namespace std;
 
+// 0, 8, 4, 6, 7, 1, 2, 9, 5, 3
 void sort(int a[], int n) {
   int start, end;
   start = 0, end = n-1;
   int tmp;
 
-  while(end - start > 1) {
+  while(end - start >= 1) {
     for(int i=end; i>=start+1; i--) {
       if(a[i-1]>a[i]) {
         tmp = a[i-1]; a[i-1] = a[i]; a[i] = tmp;
--- cpp/sorting/util.h
+++ cpp/sorting/util.h
@@ -1,1 +1,7 @@
+#ifndef BESSHO_ALGORITHM_SORTING_UTIL_H_
+#define BESSHO_ALGORITHM_SORTING_UTIL_H_
+
+// Print an array, n is the size of the array
 void show(int a[], int n);
+
+#endif  // #define BESSHO_ALGORITHM_SORTING_UTIL_H_
